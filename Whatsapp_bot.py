d4dab14aff5fde7498f5b0c210ff97974c9e88f1
René Salmon <rene.salmon@outlook.de>
2021-12-27 16:28:33 +0100

    serveral whatsapp bot solutions

---
+++ Whatsapp_bot.py
@@ -0,0 +1,5 @@
+import AppKit
+import pywhatkit
+
+# Send a WhatsApp Message to a Contact at 1:30 PM
+pywhatkit.sendwhatmsg("phone_number", "schlaf", 16, 4)
--- whatsapp.py
+++ whatsapp.py
@@ -6,8 +6,9 @@ account_sid = os.environ['TWILIO_ACCOUNT_SID']
 auth_token = os.environ['TWILIO_AUTH_TOKEN']
 client = Client(account_sid, auth_token)
 
-message = client.messages.create(body='IT WORKS!!!!',
-                                 from_='whatsapp:+14155238886',
-                                 to='whatsapp:'+ os.environ['MY_PHONE_NUMBER'])
+message = client.messages.create(
+    body='Sunday Night',
+    from_='whatsapp:+14155238886',
+    to='whatsapp:'+ os.environ['MY_PHONE_NUMBER'])
 
 print(message.sid)
